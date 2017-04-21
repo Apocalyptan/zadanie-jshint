d5309302ff6840a2c898d1c1bc63338f37cfd4e5
Adam Rudny <rudnyadam@gmail.com>
2017-04-21 13:52:15 +0200

    add comments for js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 var name = prompt("Enter your name");
 alert("Witaj, " + name);
 console.log("Witaj, " + name);
+
+// alert("TEST");
+
+/*
+	Komentarz blokowy!
+*/
+
+// Komentarz liniowy I
+// Komentarz liniowy II

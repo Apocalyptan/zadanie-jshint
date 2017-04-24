f85a6177b2cddfe188389c6b4e0e175aa6e668c3
Adam Rudny <rudnyadam@gmail.com>
2017-04-24 11:31:49 +0200

    gruntfile changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,55 +1,55 @@
 module.exports = function(grunt) {
-	
-	// Project configuration.
-    grunt.initConfig({
-      jshint: {
-        all: ['js/*.js']
-      },
 
-      sass: {
-        options: {
-          sourceMap: true
-        },
-        dist: {
-          files: {
-            'css/main.css': 'sass/main.sass'
-          },
-        }
-      },
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: ['js/*.js']
+    },
 
-      imagemin: {
-        dynamic: {
-          files: [{
-            expand: true,
-            cwd: 'images/',
-            src: ['**/*.{png,jpg,gif}'],
-            dest: 'images/build/'
-          }]
-        }
+    sass: {
+      options: {
+        sourceMap: true
       },
-
-      watch: {
-        css: {
-          files: ['sass/*.sass'],
-          tasks: ['sass'],
-          options: {
-            spawn: false,
-          },
-        },
-        js: {
-          files: ['<%= jshint.all %>'],
-          tasks: ['jshint']
+      dist: {
+        files: {
+        'css/main.css': 'sass/main.sass'
         }
       }
-    });
+    },
 
-	// Load the plugin tasks.
+    imagemin: {
+      dynamic: {
+        files: [{
+        expand: true,
+        cwd: 'images/',
+        src: ['**/*.{png,jpg,gif}'],
+        dest: 'images/build/'
+        }]
+      }
+    },
+
+    watch: {
+      css: {
+        files: ['sass/*.sass'],
+        tasks: ['sass'],
+        options: {
+        spawn: false,
+        }
+      },
+      js: {
+        files: ['<%= jshint.all %>'],
+        tasks: ['jshint']
+      }
+    }
+  });
+
+  	// Load the plugin tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-sass');
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-	// Default task(s).
-	grunt.registerTask('default', ["jshint", "sass", "imagemin", "watch"]);
+  	// Default task(s).
+  grunt.registerTask('default', ["jshint", "sass", "imagemin", "watch"]);
 
 };
